5995a6e24ed73963f71943a82bcf571139d58ebd
Peter Kalvin <petimsn@gmail.com>
2019-09-26 23:01:25 +0200

    spi

--- Marlin/src/pins/lpc1768/pins_BIGTREE_SKR_V1.3.h
+++ Marlin/src/pins/lpc1768/pins_BIGTREE_SKR_V1.3.h
@@ -62,11 +62,11 @@
 //
 // Steppers
 //
-#define Z_STEP_PIN         P2_02
-#define Z_DIR_PIN          P2_06
-#define Z_ENABLE_PIN       P2_01
-#ifndef Z_CS_PIN
-  #define Z_CS_PIN         P1_17
+#define X_STEP_PIN         P2_02
+#define X_DIR_PIN          P2_06
+#define X_ENABLE_PIN       P2_01
+#ifndef X_CS_PIN
+  #define X_CS_PIN         P1_17
 #endif
 
 #define Y_STEP_PIN         P0_19
@@ -76,26 +76,12 @@
   #define Y_CS_PIN         P1_15
 #endif
 
-// #define X_STEP_PIN         P0_01
-// #define X_DIR_PIN          P0_00
-// #define X_ENABLE_PIN       P0_10
-// #ifndef X_CS_PIN
-//   #define X_CS_PIN         P1_01
-// #endif
-
-#define X_STEP_PIN         P0_01
-#define X_DIR_PIN          P0_00
-#define X_ENABLE_PIN       P0_10
-#ifndef X_CS_PIN
-  #define X_CS_PIN         P1_01
+#define Z_STEP_PIN         P0_22
+#define Z_DIR_PIN          P2_11
+#define Z_ENABLE_PIN       P0_21
+#ifndef Z_CS_PIN
+  #define Z_CS_PIN         P1_10
 #endif
-
-// #define Z_STEP_PIN         P0_22
-// #define Z_DIR_PIN          P2_11
-// #define Z_ENABLE_PIN       P0_21
-// #ifndef Z_CS_PIN
-//   #define Z_CS_PIN         P1_10
-// #endif
 
 #define E0_STEP_PIN        P2_13
 #define E0_DIR_PIN         P0_11
@@ -104,12 +90,12 @@
   #define E0_CS_PIN        P1_08
 #endif
 
-// #define E1_STEP_PIN        P0_01
-// #define E1_DIR_PIN         P0_00
-// #define E1_ENABLE_PIN      P0_10
-// #ifndef E1_CS_PIN
-//   #define E1_CS_PIN        P1_01
-// #endif
+#define E1_STEP_PIN        P0_01
+#define E1_DIR_PIN         P0_00
+#define E1_ENABLE_PIN      P0_10
+#ifndef E1_CS_PIN
+  #define E1_CS_PIN        P1_01
+#endif
 
 //
 // Software SPI pins for TMC2130 stepper drivers
